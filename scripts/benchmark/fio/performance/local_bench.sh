deeda646054817bce24091d1e6771d970daea555
Sudarsan <appsby12@gmail.com>
2024-10-23 23:25:25 +0200

    Update local_bench.sh

--- scripts/benchmark/fio/performance/local_bench.sh
+++ scripts/benchmark/fio/performance/local_bench.sh
@@ -4,7 +4,7 @@ sudo nvme disconnect -d /dev/nvme0
 ../../../setup/nvmeof_tcp_initiator_setup.sh thesis.dev0 10 10
 
 #Setting target polling
-curl "http://172.16.137.2:8080/poll?poll=60"
+curl "http://localhost:8080/poll?poll=40"
 
 #Disabline arfs
 ../../../setup/rps.sh 0
