7f54af30fefee1e483e594b11986d26399a78338
Sudarsan <appsby12@gmail.com>
2024-10-23 23:41:28 +0200

    Update target.sh

--- scripts/benchmark/fio/qos_ns/target.sh
+++ scripts/benchmark/fio/qos_ns/target.sh
@@ -1,7 +1,7 @@
 #Continue configuration from RQ1
-curl "http://172.16.137.2:8080/poll?poll=40"
+curl "http://$1:8080/poll?poll=40"
 ../../../setup/rps.sh 0
-sudo ethtool -K ens4np0 ntuple off
+sudo ethtool -K $2 ntuple off
 
 sudo nvme disconnect -d /dev/nvme0
 ../../../setup/nvmeof_tcp_initiator_setup.sh thesis.dev0 10 10
@@ -13,6 +13,6 @@ export FDEVICE="/dev/nvme0n1"
 export BDEVICE="/dev/nvme1n1"
 export MAX_IOPS=180 #Saturation point of the resoource in KIOPS
 
-curl http://172.16.137.2:8080/rss?count=1
+curl http://$1:8080/rss?count=1
 python3 fio_runner.py remote_ID_TS_SD_split_bread_fread
-curl http://172.16.137.2:8080/rss?count=10
+curl http://$1:8080/rss?count=10
