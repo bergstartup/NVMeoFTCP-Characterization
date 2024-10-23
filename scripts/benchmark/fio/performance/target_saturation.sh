2ba66cd507f07533061d6dc650b024d9a5427730
Sudarsan <appsby12@gmail.com>
2024-10-23 23:26:45 +0200

    Update target_saturation.sh

--- scripts/benchmark/fio/performance/target_saturation.sh
+++ scripts/benchmark/fio/performance/target_saturation.sh
@@ -5,17 +5,17 @@ sudo nvme disconnect -d /dev/nvme0
 ../../../setup/nvmeof_tcp_initiator_setup.sh thesis.dev0 10 10
 
 #Set target polling
-curl "http://172.16.137.2:8080/poll?poll=40"
+curl "http://$1:8080/poll?poll=40"
 
 #Without arfs
 ../../../setup/rps.sh 0
 sudo ethtool -K ens4np0 ntuple off 
 
 #Do experiments
-curl http://172.16.137.2:8080/rss?count=1
+curl http://$1:8080/rss?count=1
 sudo python3 fio_runner.py remote_target_C1
-curl http://172.16.137.2:8080/rss?count=2
+curl http://$1:8080/rss?count=2
 sudo python3 fio_runner.py remote_target_C2
-curl http://172.16.137.2:8080/rss?count=3
+curl http://$1:8080/rss?count=3
 sudo python3 fio_runner.py remote_target_C3
-curl http://172.16.137.2:8080/rss?count=10
+curl http://$1:8080/rss?count=10
