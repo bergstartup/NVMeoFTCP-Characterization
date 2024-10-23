458fcce2bbfedf67afffdb2e448a8f8f4b738810
Sudarsan <appsby12@gmail.com>
2024-10-23 23:28:35 +0200

    Update polling_queues.sh

--- scripts/benchmark/fio/performance/polling_queues.sh
+++ scripts/benchmark/fio/performance/polling_queues.sh
@@ -1,4 +1,9 @@
+#No target polling
 curl "http://$1:8080/poll?poll=0"
+#Without arfs
+../../../setup/rps.sh 0
+sudo ethtool -K $2 ntuple off 
+
 sudo nvme disconnect -d /dev/nvme0
 ../../../setup/nvmeof_tcp_initiator_setup.sh thesis.dev0 10 0
 sudo python3 fio_runner.py remote_perf_npoll
